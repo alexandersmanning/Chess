1be5293c77051b7eba2cc3d37addc626a7c077d7
alexandersmanning <alexandersmanning@gmail.com>
2016-08-19 21:31:20 -0700

    Wrote simulate moves, add test to game

--- lib/game.rb
+++ lib/game.rb
@@ -29,15 +29,37 @@ class Game
 		Board.new.tap { |board| board.grid = mock_grid }
 	end 
 
-	def get_opponent_moves 
-		@board.piece_list(get_opponent).inject([]) do |list, locations|
-			list.push *locations
+	def get_opponent_moves(board)
+		opponent = get_opponent.color
+
+		board.piece_locations(opponent).inject([]) do |list, location|
+			list.push *board[*location].move_list(location, board)
 		end.uniq
 	end 
 
 	def simulate_moves(location) 
 		mock_board = clone_board
-		#for the piece at the location, find all moves given the mock board and then go to each move one by one to see if any cause check
+		#find the king location for the current player 
+		player = current_player.color
+		opponent = get_opponent.color
 
+		king_location = mock_board.piece_by_type(player, King)
+		#for the piece at the location, find all moves given the mock board and then go to each move one by one to see if any cause check
+		piece = mock_board[*location]
+		#now get a list of moves for that piece 
+		move_list = piece.move_list(location, board)
+		#for each move in the move list, see which ones give you opponent direct access to the king 
+		allowed_moves []
+
+		move_list.each do |new_location| 
+			mock_board.move(location, new_location)
+			opponent_moves = get_opponent_moves(opponent)
+
+			allowed_moves << new_location unless opponent_moves.include?(king_location) #this won't work if piece is king
+		end
+
+		return allowed_moves 
 	end 
+
+	##checkmate: If opponent allowed moves include the king, then see if player can move any piece to stop it 
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,25 @@
 require 'rspec'
 require 'board'
 require 'piece'
+require 'game'
 require 'byebug'
 
 describe "Game" do 
 
-	let(:game) { Game.new }
+	let(:game){ Game.new }
+	let(:board){ game.board }
+	let(:player_1){ double("player_1") }
+	let(:player_2){ double("player_2") }
 
 	before :each do 
-		board.setup
+		game.setup
+
+		allow(player_1).to receive(:color).and_return(:white)
+		allow(player_2).to receive(:color).and_return(:black)
+
+		allow(game).to receive(:player_1).and_return(player_1)
+		allow(game).to receive(:player_2).and_return(player_2)
+		allow(game).to receive(:current_player).and_return(player_1)
 	end 
 
 	describe "#in_check" do 
@@ -30,15 +41,34 @@ describe "Game" do
 
 	end 
 
+	describe "#get_opponent" do 
+		it "returns the opponent player" do 
+			expect(game.get_opponent).to eq player_2
+		end 
+	end 
+
 	describe "#clone_board" do 
 		it "takes each row of a boards grid and writes it to a new board's grid" do 
 			dup_board = game.clone_board 
+			expect(board.grid).to match dup_board.grid
 		end 
 	end 
 
 	describe "#get_opponent_moves" do 
 		it "gets a list of all the moves each opponent piece has" do 
-			ex
+			move_list = []
+			8.times { |n| move_list.push([4, n], [5, n ]) }
+
+			expect(game.get_opponent_moves(board).sort).to match move_list.sort
+		end 
+
+		it "gets a list of all the moves for different players" do 
+			allow(game).to receive(:current_player).and_return(player_2)
+
+			move_list = []
+			8.times { |n| move_list.push([2, n], [3, n ]) }
+
+			expect(game.get_opponent_moves(board).sort).to match move_list.sort
 		end 
 	end 
 end 
