ec10428178d43820f880a9fe7afd6ab30f171b1c
alexandersmanning <alexandersmanning@gmail.com>
2016-08-28 14:54:42 -0700

    completed save, back, and exit

--- lib/game.rb
+++ lib/game.rb
@@ -6,50 +6,64 @@ require_relative 'piece'
 class Game 
 	attr_accessor :board, :players, :current_player
 	def initialize
-		@players = []
-	end 
-
-	def setup 
-		@board = Board.new
-		@board.setup
-
-		get_players
+		@check = false
 	end 
 
 	def play_game 
 		setup 
-		until (check = in_check?(board)) && check_mate?
-			puts "You are in check" if check
+		until (@check = in_check?(board)) && check_mate?
 			turn 
 			switch_players!
 		end 
 	end 
 
+	def setup 
+		system('clear')
+		puts "Welcome to Chess\nDo you want to start a new game or continue a previous game? ('N' for new, 'L' for load)"
+
+		input = gets.chomp.scan(/[lL]/).first
+		if !input.nil? && input.upcase == 'L'
+			game = load_game_data
+			@board = game.board 
+			@players = game.players 
+			@current_player = game.current_player
+		else
+			@board = Board.new
+			@board.setup
+			get_players
+		end 
+	end 
+
 	def turn 
+		piece = nil 
+
 		begin 
 			system("clear")
+			puts @board.display(current_player.color)
+			puts "You are in check\n" if @check
+			puts "#{@current_player.name.capitalize}: Enter the coordinates for the piece you would like to move (e.g. 3C), or a command (Save, Exit)"
+			
 			input = get_input 
 			raise if input == "back"
 			location, allowed_moves = *get_piece(input)
 
+			piece = board[*location].class
+			puts "Enter where you want to move your #{piece}\nor enter Save, Back, or Exit\r"
 			input = get_to_location(allowed_moves)
+
 			raise if input == "back"
 			to_location = input
 		rescue 
 			retry 
 		end 
 
-		piece = board[*location].class
 		captured = board.move(location, to_location)
 
 		puts "#{current_player.color.to_s.capitalize} #{piece} moved from #{location_output(location)} to #{location_output(to_location)}"
 		puts "#{get_opponent.color.to_s.capitalize} #{captured.class} was captured" unless captured.nil?
-		sleep(1)
 	end 
 
-	def get_input 
-		puts @board.display(current_player.color)
-		puts "#{@current_player.name}: Enter the coordinates for the piece you would like to move (e.g. 3C), or a command (save, back, exit)"
+	def get_input(in_check = false) 
 		analyze_input(current_player.get_move)
 	end 
 
@@ -58,18 +72,19 @@ class Game
 			location = convert_location(input)
 			allowed_part = !@board[*location].nil? && @board[*location].player == @current_player.color
 			raise "This is not a valid location" unless allowed_part
-
 			allowed_moves = simulate_moves(location)
 			raise  "This piece has no available moves" if allowed_moves.empty?
 			return [location, allowed_moves]
 		rescue Exception => e
 			puts e.message
-			get_input
+			input = ""
+			puts "Please enter piece coordinates"
+			input = get_input until input.is_a?(Array)
+			retry 
 		end 
 	end 
 
 	def get_to_location(allowed_moves) 
-		puts "Please enter where you would like to move the piece"
 		begin 
 			input = analyze_input(current_player.get_move)
 			return input if input == 'back'
@@ -94,13 +109,14 @@ class Game
 
 	def save_game
 		puts "game saved!"
-		#call yaml 
+		save_game_data
+		sleep(1)
 		return "back"
 	end 
 
 	def exit_game 
 		puts "Are you sure you want to leave?"
-		input = gets.chomp(/[nN]/).first 
+		input = gets.chomp.scan(/[nN]/) 
 		exit if input.empty?
 		return "back"
 	end 
@@ -177,23 +193,38 @@ class Game
 		time_string = "#{time.year}#{time.month}#{time.day}_#{time.hour}#{time.min}"
 		
 		Dir.mkdir("file") unless File.directory?("file")
-		File.open("files/chess_game_#{time_string}.txt", 'w') { |f| f.write(YAML.dump(self)) }
+		File.open("file/chess_game_#{time_string}.txt", 'w') do |f| 
+			f.write(YAML.dump(self))
+		end 
 	end 
 
 	def load_game_data
 		puts "Please enter a number corresponding to the game you want to load"
-		Dir.glob('file/chess_game_*.txt').each_with_index do |fname, idx|
+		file_list = Dir.glob('file/chess_game_*.txt')
+		input = display_files(file_list)
+		YAML.load(File.read(file_list[input]))
+	end 
+
+	def display_files(file_list)
+		count = 0
+		file_list.each_with_index do |fname, idx|
 			puts "#{idx + 1}: #{fname.gsub(/file\//,"")}"
-			count = 1 || count + 1
+			count += 1
 		end 
 
 		begin 
-			input = gets.chomp.scan(/\d/).to_i
+			input = gets.chomp.scan(/\d/).first.to_i
+			raise unless input.between?(1, count)
 		rescue 
 			puts "Please enter a number between 1 and #{count}"
+			retry 
 		end 
+
+		return input - 1
 	end 
+
+
 end 
 
-game = Game.new 
-game.play_game
+# game = Game.new 
+# game.play_game
